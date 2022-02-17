35cb1e9c666dc90625c67f9a94d27d3e65f83aca
Jasuv <ejasuv@gmail.com>
2022-02-16 21:24:50 -0800

    testing change

--- Scripts/StaticVariables.cs
+++ Scripts/StaticVariables.cs
@@ -4,6 +4,6 @@ using UnityEngine;
 
 public static class StaticVariables
 {
-    public static float gravityConstant = 1f;
+    public static float gravityConstant = 1f ;
     public static float physicsTimeScale = .1f;
 }
